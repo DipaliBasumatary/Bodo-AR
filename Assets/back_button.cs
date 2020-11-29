860e2a9a128e8608ff3ae75c9c0ae03f6921033c
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2020-11-30 02:18:58 +0530

    Initial commit

---
+++ Assets/back_button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back_button : MonoBehaviour
+{
+    public void goBack()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+}
---
+++ Assets/main_menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour
+{
+    public void exitButton()
+    {
+        Application.Quit();
+        Debug.Log("Exit Done!");
+    }
+    
+    public void startAR()
+    {
+        SceneManager.LoadScene("vowels");
+    }
+}
